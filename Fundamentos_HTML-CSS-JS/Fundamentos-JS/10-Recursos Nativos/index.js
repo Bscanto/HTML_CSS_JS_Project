7c7b24fc9a8d3ad52cf9a7b9130c0a9bc45e2664
Bruno Canto <bruno_scanto@hotmail.com>
2024-12-17 17:25:27 -0300

    introdução a JS

---
+++ Fundamentos_HTML-CSS-JS/Fundamentos-JS/10-Recursos Nativos/index.js
@@ -0,0 +1,12 @@
+let nome = "Bruno"
+
+console.log(nome)
+console.error(nome)
+console.warn(nome)
+
+// window.alert(nome)
+//nome = window.prompt("Insira o seu nome: ")
+//let confirmacao = window.confirm("confirma essa ação?")
+
+//console.log(confirmacao)
+alert("Olá Mundo")
---
+++ Fundamentos_HTML-CSS-JS/Fundamentos-JS/9-JS-n-nHTML/index.js
@@ -0,0 +1,5 @@
+let name = "Bruno"
+let x = 5
+
+console.log(name)
+console.log(x)
