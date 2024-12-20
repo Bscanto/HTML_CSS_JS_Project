45cf721a5ea9ce0ecf672fe576411848e2b4dbf8
Bruno Canto <bruno_scanto@hotmail.com>
2024-12-20 16:27:51 -0300

    switch

---
+++ Fundamentos_HTML-CSS-JS/Fundamentos-JS/19-Switch/switch.js
@@ -0,0 +1,17 @@
+const resultado = prompt("Escolha uma Alternativa: \na)\nb)\nc)")
+
+
+
+switch (resultado){
+  case "a":
+    alert("O resultado é 'a'")
+    break
+  case "b":
+    alert("O resultado é 'b'")
+    break
+  case "c":
+    alert("O resultado é 'c'")
+    break
+  default:
+    alert("Finalizando...")
+}
---
+++ Fundamentos_HTML-CSS-JS/Fundamentos-JS/exercicio-3/index.js
@@ -0,0 +1,19 @@
+let metros = parseFloat(prompt("Digite um valor em metros:"));
+
+const converssão = prompt("Escolha uma Alternativa: \n1) milímetros(mm)\n2) centimetros(cm)\n3) decímetro(dm)\n4) decâdametro(dam)\n5) hectômetro(hm)\n6) quilômetro(km)")
+
+
+
+switch (resultado){
+  case "a":
+    alert("O resultado é 'a'")
+    break
+  case "b":
+    alert("O resultado é 'b'")
+    break
+  case "c":
+    alert("O resultado é 'c'")
+    break
+  default:
+    alert("Finalizando...")
+}
