7996354d0885725bb190f3468b8d70d1e6dee954
Bruno Canto <bruno_scanto@hotmail.com>
2024-12-23 21:41:46 -0300

    finalizando JS I

---
+++ Fundamentos_HTML-CSS-JS/Fundamentos-JS/exercicio-6/palindromo.js
@@ -0,0 +1,13 @@
+const palavra = prompt("Informe uma palavra")
+
+let palavraInvertida = ""
+
+for (let i = palavra.length - 1; i >= 0; i--) {
+  palavraInvertida += palavra[i]
+}
+
+if (palavra === palavraInvertida){
+  alert(palavra + " é um palíndromo")
+} else {
+  alert(palavra + " não é um palíndromo")
+}
