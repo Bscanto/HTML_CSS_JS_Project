e44e2bc3d24f01fad01262d8aef8a22d3c9bf5a2
Bruno Canto <bruno_scanto@hotmail.com>
2024-12-27 14:17:12 -0300

    arrays

---
+++ Fundamentos_HTML-CSS-JS/Fundamentos-JS-II/arrays-bidimensionais/matrizes.js
@@ -0,0 +1,15 @@
+const arr = [
+  "1º Nível",
+  ["2º nível", 42, true],
+  [
+    ["3º nível, 1º item", "Olá, mundo!"],
+    ["3º nível, 2º item", "Oi, mundo!"],
+  ],
+  []
+]
+
+console.log(arr)
+console.log(arr[0])
+console.log(arr[1])
+console.log(arr[1][0])
+console.log(arr[1][1])
