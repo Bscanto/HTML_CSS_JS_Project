1c47b41fea85ac5b323efb4de576376974b12a16
Bruno Canto <bruno_scanto@hotmail.com>
2025-02-24 16:36:20 -0300

    Estudo Função recursiva

---
+++ Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-II/06-funcao-recursiva/recursao.js
@@ -0,0 +1,58 @@
+// Função recursiva é uma função que chama ela mesma repetidamente
+function dividir(num){
+  console.log(num)
+  if(num % 2 ==0){
+    dividir(num / 2)
+  }else {
+    return num
+  }
+}
+
+dividir(256)
+
+// PERIDO DAS FUNÇÕES RECURSIVAS
+// É fundamental que uma função recursiva tenha uma forma de parar de chamar novas funções
+function dobrar(num) {
+  console.log(num)
+  dobrar(num * 2)
+}
+//dobrar(1) // Estoura a pilha de chamadas
+
+// Escrevendo Funções Recursivas
+// Para construir uma função recursiva, geralmente partimos de um caso de base
+// para garantir que nossa função não vai continuar se chamando para sempre
+// Obs.: !5 (fatorial de 5) = 5 * 4 * 3 * 2 * 1 = 5 * !4
+function fatorial(num) {
+  console.log("num = " + num)
+  if (num === 0) { // caso base
+    return 1
+  } else if (num === 1) { // caso base
+    return 1
+  } else {
+    console.log(num + " * !" + (num - 1))
+    return num * fatorial(num - 1)
+  }
+}
+
+console.log("\n!5 = " + fatorial(5))
+console.log("\n!0 = " + fatorial(0))
+console.log("\n!9 = " + fatorial(9))
+
+
+
+
+
+
+
+
+
+
+// Sequência de Fibonacci
+// A sequência de Fibonacci segue a relação: F(n) = F(n - 1) + F(n - 2)
+console.log('Sequência de Fibonassi')
+function fibonacci(n) {
+  if (n <= 0) return 0;
+  if (n === 1) return 1;
+  return fibonacci(n - 1) + fibonacci(n - 2);
+}
+console.log(fibonacci(6)); // 8
