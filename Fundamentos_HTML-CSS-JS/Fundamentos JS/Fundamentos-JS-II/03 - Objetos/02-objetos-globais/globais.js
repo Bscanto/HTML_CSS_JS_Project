eaf6667a98a3d6422f8af1fb5d9f72093ae1c14c
Bruno Canto <bruno_scanto@hotmail.com>
2025-03-03 17:30:21 -0300

    objetos globais

---
+++ Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-II/03 - Objetos/02-objetos-globais/globais.js
@@ -0,0 +1,13 @@
+/* OBJETOS GLOBAIS
+São objetos padrão da linguagem javascript que ficam sempre disponiveis para utilizarmos.
+ATENÇÂO: não confundir com o objeto de escopo global window do navegador(ou escopo global de outros ambientes.)
+Alguns objetos globais:
+Objeto-
+Array-
+String-
+Number-
+Date-
+Math-
+
+Podem ser usados para criar e converter valores
+*/
---
+++ Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-II/06-desafio-final/sistemaVaga.js
@@ -0,0 +1,139 @@
+/*
+Sistema de Vagas de Emprego
+
+Escreva um programa em javascript que simule um sistema de vagas de emprego, onde é possível gerenciar as vagas e adicionar candidatos às vagas. Ele deve atender aos seguintes requisitos:
+
+Ter um um menu onde é possível escolher entre as diferentes funcionalidades do sistemaListar vagas disponíveisCriar um nova vagaVisualizar uma vagaInscrever um candidato em uma vagaExcluir uma vagaSair
+
+- Listar vagas disponíveis
+- Criar um nova vaga
+- Visualizar uma vaga
+- Inscrever um candidato em uma vaga
+- Excluir uma vaga
+- Sair
+
+A opção de listar as vagas deve mostrar o índice, o nome e a quantidade de candidatos inscritos de todas as vagas.
+
+A opção de criar uma nova vaga deve pedir um nome para a vaga, uma descrição e uma data limite, e também deve pedir que o usuário confirme as informações antes de salvá-las.
+
+A opção de visualizar uma vaga deve pedir o índice da vaga e mostrar todas as informações dela: índice, nome, descrição, data limite, quantidade de candidatos e o nome dos candidatos.
+
+A opção de inscrever um candidato em uma vaga de pedir o nome do candidato, o índice da vaga e então uma confirmação exibindo as informações da vaga antes de salvar o candidato na vaga.
+
+A opção de excluir uma vaga deve pedir o índice da vaga, mostrar suas informações e pedir que o usuário confirme a exclusão da vaga antes de realmente exclui-la.
+
+Este é o exercício de revisão do módulo, então aproveite para utilizar todos os recursos vistos até agora sempre que possível, como os objetos, arrays e funções.
+*/
+
+const vagas = []
+
+function exibirMenu(){
+  opcao = prompt(
+    "Cadastro de Vagas de Emprego" +
+    "\n\nEscolha uma das opções" +
+    "\n1. Listar vagas disponíveis" +
+    "\n2. Criar uma nova vaga" +
+    "\n3. Visualizar uma vaga" +
+    "\n4. Inscrever um(a) candidato(a)" +
+    "\n5. Excluir uma vaga" +
+    "\n6. Sair"
+  )
+  return opcao
+  
+} 
+
+function executar() {
+  do {
+    opcao = exibirMenu()
+
+    switch (opcao) {
+      case "1":
+        listarVagas()
+        break
+      case "2":
+        novaVaga()
+        break
+      case "3":
+        exibirVaga()
+        break
+      case "4":
+        inscreverCandidato()
+        break
+      case "5":
+        excluirVaga()
+      case "6":
+        alert("Saindo...")
+        break
+      default:
+        alert("Opção inválida.")
+    }
+
+  } while (opcao !== "6");
+}
+
+executar()
+
+
+//Função Listar vagas:
+//A opção de listar as vagas deve mostrar o índice, o nome e a quantidade de candidatos inscritos de todas as vagas.
+function listarVagas() {
+
+  const vagasEmTexto = vagas.reduce((textoFinal, vaga, indice) => {
+    textoFinal += indice + ". "
+    textoFinal += vaga.nome
+    textoFinal += " (" + vaga.candidatos.length + " candidatos)\n"
+    return textoFinal
+  }, "")
+
+  if(vagas.length === 0 ){
+    alert("Não há vagas cadastradas.")
+  }else {
+  alert(vagasEmTexto)
+  }
+}
+
+
+// Função Nova Vaga(cadastrar):
+//A opção de criar uma nova vaga deve pedir um nome para a vaga, uma descrição e uma data limite, e também deve pedir que o usuário confirme as informações antes de salvá-las.
+function novaVaga() {
+const nome = prompt("Informe um nome para a vaga: ")
+const descrição = prompt("Informe uma descrição para a vaga: ")
+const dataLimite = prompt("Informe uma data limite (dd/mm/aaaa) para a vaga: ")
+
+const confirmacao = confirm(
+  "Deseja Criar uma nova vaga com essas informações?" +
+  "\nNome: " + nome + 
+  "\nDescrição: " + descrição +
+  "\nData Limite: " + dataLimite
+)
+
+if( confirmacao) {
+  const novaVaga = {nome, descrição, dataLimite, candidatos: [] }
+  vagas.push(novaVaga)
+  alert("Vaga Criada")
+}
+}
+
+// Função visualizar uma vaga
+//A opção de visualizar uma vaga deve pedir o índice da vaga e mostrar todas as informações dela: índice, nome, descrição, data limite, quantidade de candidatos e o nome dos candidatos.
+function exibirVaga() {
+
+}
+
+
+
+// Função Inscrever um candidato
+//A opção de inscrever um candidato em uma vaga de pedir o nome do candidato, o índice da vaga e então uma confirmação exibindo as informações da vaga antes de salvar o candidato na vaga.
+function inscreverCandidato() {
+
+}
+
+
+
+
+// Função excluir uma vaga
+//A opção de excluir uma vaga deve pedir o índice da vaga, mostrar suas informações e pedir que o usuário confirme a exclusão da vaga antes de realmente exclui-la.
+function excluirVaga() {
+
+}
+
