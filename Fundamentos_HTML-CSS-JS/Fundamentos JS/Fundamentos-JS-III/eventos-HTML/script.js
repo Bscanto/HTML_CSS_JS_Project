5dda74208bf3903c413413a2930fef73eb45ea7b
Bruno Canto <bruno_scanto@hotmail.com>
2025-03-12 22:35:54 -0300

    Dom

--- Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/eventos-HTML/script.js
+++ Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/eventos-HTML/script.js
@@ -2,17 +2,21 @@ function mouseEnterEvent() {
   console.log('mouse entered')
 }
 
+
 function changeEvent() {
   console.log('value changed')
 }
+
 
 function focusEvent() {
   console.log('gained focus')
 }
 
+
 function blurEvent() {
   console.log('lost focus')
 }
+
 
 function clickEvent() {
   alert('clicked')
