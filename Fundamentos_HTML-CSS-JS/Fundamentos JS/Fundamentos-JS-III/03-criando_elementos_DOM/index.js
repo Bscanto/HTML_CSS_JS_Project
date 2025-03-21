50bf72b0b313b115bb961100710c7ca867f146b2
Bruno Canto <bruno_scanto@hotmail.com>
2025-03-21 20:20:52 -0300

    criando elementod dom

--- Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/03-criando_elementos_DOM/index.js
+++ Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/03-criando_elementos_DOM/index.js
@@ -7,8 +7,17 @@ function addInput() {
 
   const newInput = document.createElement('input')
   newInput.type = 'text'
-  newInput.name = 'input'
+   newInput.name = `input-${document.querySelectorAll('.list-item').length + 1}`
+  newInput.placeholder = 'Digite algo...'
+
+  const removeButton = document.createElement('button')
+  removeButton.innerText = 'Remover'
+  removeButton.onclick = () => newLi.remove()
+
 
   newLi.appendChild(newInput)
+  
+  newLi.appendChild(removeButton)
   ul.appendChild(newLi)
 }
+
