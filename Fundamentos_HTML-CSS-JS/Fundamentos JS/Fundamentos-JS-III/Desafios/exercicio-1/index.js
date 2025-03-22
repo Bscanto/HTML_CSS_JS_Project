efdfcc9796cacd0babf636b0df6921ade9815ac1
Bruno Canto <bruno_scanto@hotmail.com>
2025-03-21 22:08:05 -0300

    exercicio manipulação dom

--- Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/Desafios/exercicio-1/index.js
+++ Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/Desafios/exercicio-1/index.js
@@ -45,3 +45,36 @@ Construa uma página web que permita montar a escalação de um time de jogadore
 
 Dica: lembrando que é possível acessar o texto de um input através da propriedade value.
 */
+
+function addPlayer() {
+  const position = document.getElementById('position').value
+  const name = document.getElementById('name').value
+  const number = document.getElementById('number').value
+
+  const confirmation = confirm("Escalar " + name + " como " + position + "?")
+
+  if(confirmation) {
+    const teamList = document.getElementById('teamList')
+    const playerItem =document.createElement('li')
+    playerItem.id ='Player- ' + number
+    playerItem.innerText = position + ": " + name + " (" + number + ")"
+
+    teamList.appendChild(playerItem)
+
+    document.getElementById('position').value = ""
+    document.getElementById('name').value = ""
+    document.getElementById('number').value = ""
+  }
+}
+
+function removePlayer() {
+  const number = document.getElementById('numberToRemove').value
+  const playerToRemove = document.getElementById('Player- ' + number)
+
+  const confirmation = confirm("Remover o jogador " + playerToRemove.innerText + "?")
+  if(confirmation){
+    document.getElementById('teamList').removeChild(playerToRemove)
+    document.getElementById('numberToRemove').value = ""
+  }
+
+}
