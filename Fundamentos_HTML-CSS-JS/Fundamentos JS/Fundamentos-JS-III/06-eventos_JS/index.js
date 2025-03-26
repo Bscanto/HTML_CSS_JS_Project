62af1c18d1d198617b240751a6d80fa6a7606268
Bruno Canto <bruno_scanto@hotmail.com>
2025-03-26 12:31:14 -0300

    eventos no JS

---
+++ Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/06-eventos_JS/index.js
@@ -0,0 +1,15 @@
+const button = document.getElementById('register-button')
+
+button.addEventListener()
+
+function register(element) {
+  const username = element.children.username.value
+  const password = element.children.password.value
+  const passwordConfirmation = element.children.passwordConfirmation.value
+
+  if (password === passwordConfirmation) {
+    alert("Usuário " + username + " registrado!")
+  } else {
+    alert("As senhas não conferem")
+  }
+}
