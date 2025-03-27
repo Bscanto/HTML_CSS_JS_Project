39ceb0ecc5a51139cd19a71973d302a0260be623
Bruno Canto <bruno_scanto@hotmail.com>
2025-03-27 18:49:37 -0300

    exercicio 12 resolvido

--- Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/Desafios/exercicio-2/index.js
+++ Fundamentos_HTML-CSS-JS/Fundamentos JS/Fundamentos-JS-III/Desafios/exercicio-2/index.js
@@ -1,80 +1,71 @@
-/*
-## Cadastro de Devs
-
-Construa uma página web que permita cadastrar desenvolvedores, suas tecnologias e tempos de experiência. 
-
-- A página deve ter um input para o nome completo do desenvolvedor e um botão para adicionar uma nova tecnologia.
-- Ao clicar no botão um evento deve ser disparado criando no formulário uma nova linha de inputs contendo:
-    - um campo de texto para o nome da tecnologia
-    - um grupo de botões tipo “radio” com opções de tempo de experiência (por exemplo, “0-2 anos”, “3-4 anos” e “5+ anos”)
-    - um botão de remover que deve excluir essa linha.
-- O formulário também deve ter um botão de cadastrar que salva as informações do dev em um array e limpa o formulário. Para testar o funcionamento você pode utilizar um simples console.log()
-
-Para esse exercício você deve utilizar apenas eventos adicionados via javascript e a página deve funcionar sem acionar um recarregamento.
-__________________________________________________
-1. Criar a Estrutura HTML
-Primeiro, precisamos da estrutura básica da página, que inclui:
-
-Um input para o nome do desenvolvedor.
-
-Um botão para adicionar tecnologias.
-
-Um botão de cadastrar.
-
-Um local onde as tecnologias serão adicionadas dinamicamente.
-
-Pergunta: Você quer que eu escreva o HTML inicial para você ou prefere tentar e me mostrar?
-_____________________________________________________
-2. Criar o Script JS e Adicionar Eventos
-Precisamos adicionar eventos para:
-
-Capturar o clique no botão de adicionar tecnologia.
-
-Criar dinamicamente os inputs necessários.
-
-Criar um botão para remover a tecnologia.
-
-Tarefa: Como você imagina fazer isso? Podemos começar com um addEventListener no botão de adicionar tecnologia.
-__________________________________________________________
-3. Remover uma Linha de Tecnologia
-Cada linha de tecnologia deve ter um botão de remover que, ao ser clicado, apaga apenas essa linha.
-
-Tarefa: Como podemos identificar qual linha deve ser removida?
-_________________________________________________________
-4. Cadastrar o Desenvolvedor
-Quando o usuário clicar no botão "Cadastrar":
-
-Capturamos os dados do formulário.
-
-Salvamos as informações em um array.
-
-Limpamos o formulário.
-*/
-
 const devs = [];
-      
+
 function addTech() {
-    const ul = document.getElementById('techList')
-
-    const newLi = document.createElement('li')
-    newLi.className = 'list-item'
-    newLi.innerText = 'Adicionar Tecnologia: '
-  
-    const newInput = document.createElement('input')
-    newInput.type = 'text'
-     newInput.name = `input-${document.querySelectorAll('.list-item').length + 1}`
-    newInput.placeholder = 'Digite uma tecnologia...'
-  
+    const techList = document.getElementById("techList");
+    const li = document.createElement("li");
 
 
+    const input = document.createElement("input");
+    input.type = "text";
+    input.placeholder = "Tecnologia";
+    input.required = true;
 
-    const removeButton = document.createElement('button')
-    removeButton.innerText = 'Remover'
-    removeButton.className = 'btn-red'
-    removeButton.onclick = () => newLi.remove()
-  
-  
-    newLi.appendChild(newInput)
-    newLi.appendChild(removeButton)
-    ul.appendChild(newLi)
-  }
+
+    const expGroup = document.createElement("div");
+    ["0-2 anos", "3-4 anos", "5+ anos"].forEach(exp => {
+        const label = document.createElement("label");
+        const radio = document.createElement("input");
+        radio.type = "radio";
+        radio.name = `exp-${techList.children.length}`; 
+        radio.value = exp;
+        label.appendChild(radio);
+        label.appendChild(document.createTextNode(exp));
+        expGroup.appendChild(label);
+    });
+
+
+    const removeBtn = document.createElement("button");
+    removeBtn.innerText = "Remover";
+    removeBtn.type = "button";
+    removeBtn.className = "btn-remove"
+    removeBtn.onclick = () => techList.removeChild(li);
+
+    li.appendChild(input);
+    li.appendChild(expGroup);
+    li.appendChild(removeBtn);
+    techList.appendChild(li);
+}
+
+
+document.getElementById("devForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    const devName = document.getElementById("devName").value;
+    const techItems = document.querySelectorAll("#techList li");
+
+    const dev = { name: devName, technologies: [] };
+
+    techItems.forEach(item => {
+        const techName = item.querySelector("input[type='text']").value;
+        const exp = item.querySelector("input[type='radio']:checked");
+
+        if (techName && exp) {
+            dev.technologies.push({ name: techName, experience: exp.value });
+        }
+    });
+
+    devs.push(dev);
+    updateDevList();
+    this.reset();
+    document.getElementById("techList").innerHTML = "";
+});
+
+function updateDevList() {
+    const devList = document.getElementById("devList");
+    devList.innerHTML = "";
+    devs.forEach(dev => {
+        const li = document.createElement("li");
+        li.innerText = `${dev.name}: ${dev.technologies.map(t => `${t.name} (${t.experience})`).join(", ")}`;
+        devList.appendChild(li);
+    });
+}
